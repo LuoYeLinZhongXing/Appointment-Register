53c4a7715585f1e0fbf3daa611a358e09bb6292c
LuoYeYinZhongXing <anlinwei_2022@qq.com>
2026-01-18 10:53:26 +0800

    初始代码

---
+++ src/main/java/com/luoye/appointmentregister/AppointmentRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.luoye.appointmentregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentRegisterApplication.class, args);
+    }
+
+}
